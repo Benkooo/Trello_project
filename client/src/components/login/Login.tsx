74d5795d71e8bd12499bee0699742234b6f7f6f1
Arnaud <arnaud.motte@epitech.eu>
2020-05-06 21:06:53 +0200

    Login and Register linked with server ?

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,24 +1,37 @@
 import React, {useState} from 'react';
-import {Button, TextField, Typography} from "@material-ui/core";
+import {Button, CircularProgress, createStyles, Divider, TextField, Typography} from "@material-ui/core";
 import {sha256} from "js-sha256";
 import {LoginResponse} from "../../interfaces/requests";
+import {makeStyles} from "@material-ui/styles";
+// import {green} from "@material-ui/core/colors";
+
+const useStyles = makeStyles(createStyles({
+        buttonProgress: {
+            position: 'absolute'
+        },
+        textField: {
+            marginTop: "10px",
+            marginBottom: "10px"
+        }
+    }),
+);
 
 interface Props {
     setDisplayRegister: (b: boolean) => void;
 }
 
-const requestLogin = async (username: string, password: string) => {
-    console.log("TEST")
-    var headers = new Headers();
+const requestLogin = async (email: string, password: string) => {
+    console.log(email + "    " + password);
+    const headers = new Headers();
     headers.append("Content-Type", "application/json");
 
-    var raw = JSON.stringify({
-        username: username,
+    const raw = JSON.stringify({
+        email: email,
         password: password
     });
     try {
-        var response = (await (
-            await fetch("http://localhost:8080/login", {
+        const response = (await (
+            await fetch("http://localhost:5000/login", {
                 method: "POST",
                 body: raw,
                 headers,
@@ -26,12 +39,15 @@ const requestLogin = async (username: string, password: string) => {
             })
         ).json()) as LoginResponse;
         if (response.success) {
+            alert("ça fonctionne");
             // SUCCESS
         } else {
+            alert("error")
             // ERROR
         }
     } catch (error) {
         // ERROR
+        alert("error")
         console.log(error);
     }
 };
@@ -39,9 +55,22 @@ const requestLogin = async (username: string, password: string) => {
 const Login = (props: Props) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const classes = useStyles();
+    const timer = React.useRef<any>();
+    const formValid = (username != "" && password != "")
+
+    const handleButtonClick = () => {
+        if (!loading) {
+            setLoading(true);
+            timer.current = setTimeout(() => {
+                setLoading(false);
+            }, 2000);
+        }
+    };
 
     return (
-        <div style={{ width: "100vh" }}>
+        <div style={{ width: "40vh", margin: "20px"}}>
             <div
                 style={{
                     display: "flex",
@@ -49,39 +78,48 @@ const Login = (props: Props) => {
                     justifyContent: "space-between",
                 }}
             >
-                <Typography variant="h4" gutterBottom>
-                    Login
+                <Typography variant="h6" gutterBottom style={{marginBottom: "20px", color: "grey"}}>
+                    Se connecter à EpiTrello
                 </Typography>
+                {loading && <CircularProgress size={48} className={classes.buttonProgress}/>}
                 <TextField
+                    className={classes.textField}
                     id="standard-basic"
-                    label="Username or email"
+                    label="E-mail"
                     autoFocus
                     value={username}
                     onChange={(sender: any) => setUsername(sender.target.value)}
                 />
                 <TextField
+                    className={classes.textField}
                     id="standard-password-input"
                     type="password"
-                    label="Password"
-                    style={{ marginTop: "40px" }}
+                    label="Mot de passe"
                     value={password}
                     onChange={(sender: any) => setPassword(sender.target.value)}
                 />
                 <div>
-                    <Button
-                        color="primary"
-                        style={{ marginTop: "40px", justifyContent: "center" }}
-                        onClick={() => requestLogin(username, sha256(password))}
-                    >
-                        Login
-                    </Button>
+                    <div>
+                        <Button
+                            disabled={!formValid}
+                            color="primary"
+                            style={{ marginTop: "20px", justifyContent: "center" }}
+                            onClick={() =>  {
+                                handleButtonClick();
+                                requestLogin(username, sha256(password))
+                            }}
+                        >
+                            Se connecter
+                        </Button>
+                    </div>
+                    <Divider variant={"middle"}/>
                     <div>
                         <Button
                             color="primary"
-                            style={{ marginTop: "10px" }}
+                            style={{ marginTop: "20px", fontSize: "12px"}}
                             onClick={() => props.setDisplayRegister(true)}
                         >
-                            Register >
+                            Inscivez-vous à un compte
                         </Button>
                     </div>
                 </div>
--- client/src/components/login/Register.tsx
+++ client/src/components/login/Register.tsx
@@ -1,52 +1,87 @@
-import {Button, TextField, Typography} from "@material-ui/core";
+import {Button, CircularProgress, createStyles, Divider, TextField, Typography} from "@material-ui/core";
 import React, {useState} from "react";
 import {RegisterResponse} from "../../interfaces/requests";
 import {sha256} from "js-sha256";
+import {makeStyles} from "@material-ui/styles";
 
+const useStyles = makeStyles(createStyles({
+        buttonProgress: {
+            position: 'absolute'
+        },
+        textField: {
+            marginTop: "10px",
+            marginBottom: "10px"
+        }
+    }),
+);
 
 interface Props {
     setDisplayRegister: (b: boolean) => void;
 }
 
-const requestRegister = async (username: string, password: string, cPassword: string) => {
-    if (username === "" || password === "" || cPassword === "" || password != cPassword)
+const requestRegister = async (email: string, username: string, password: string, cPassword: string) => {
+    if (email === "" || username === "" || password === "" || cPassword === "" || password != cPassword)
     {
         // PROBLEME
         return;
     }
+
     const headers = new Headers();
     headers.append("Content-Type", "application/json");
 
+    console.log("OK")
     const raw = JSON.stringify({
         username: username,
+        email: email,
         password: password
     });
     try {
+        console.log("YESS")
+        console.log(raw)
         const response = (await (
-            await fetch("http://localhost:8080/register", {
+            await fetch("http://localhost:5000/register", {
                 method: "POST",
                 body: raw,
                 headers
             })
         ).json()) as RegisterResponse;
+        console.log(raw)
+        console.log(response)
         if (response.success) {
-            // SUCCESS;
+            alert("ça fonctionne");
+            // SUCCESS
         } else {
-            // ERROR;
+            alert("error connection")
+            // ERROR
         }
     } catch (error) {
         // ERROR;
+        alert("error")
         console.log(error);
     }
 };
 
 const Register = (props: Props) => {
+    const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [cPassword, setCPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const classes = useStyles();
+    const timer = React.useRef<any>();
+    const formValid = (username != "" && password != "" && cPassword != "" && email != "")
+
+    const handleButtonClick = () => {
+        if (!loading) {
+            setLoading(true);
+            timer.current = setTimeout(() => {
+                setLoading(false);
+            }, 2000);
+        }
+    };
 
     return (
-        <div style={{width: "100vh"}}>
+        <div style={{ width: "40vh", margin: "20px"}}>
             <div
                 style={{
                     display: "flex",
@@ -54,52 +89,67 @@ const Register = (props: Props) => {
                     justifyContent: "space-between"
                 }}
             >
-                <Typography variant="h4" gutterBottom>
-                    Register
+                <Typography variant="h6" gutterBottom style={{marginBottom: "20px", color: "grey"}}>
+                    Inscivez-vous à un compte
                 </Typography>
+                {loading && <CircularProgress size={48} className={classes.buttonProgress}/>}
                 <TextField
-                    id="standard-basic"
-                    label="Username or email"
+                    required
+                    className={classes.textField}
+                    id="emailRegister"
+                    label="E-mail"
                     autoFocus
+                    value={email}
+                    onChange={(sender: any) => setEmail(sender.target.value)}
+                />
+                <TextField
+                    required
+                    className={classes.textField}
+                    id="nameRegister"
+                    label="Nom complet"
                     value={username}
                     onChange={(sender: any) => setUsername(sender.target.value)}
                 />
                 <TextField
+                    required
+                    className={classes.textField}
                     id="standard-password-input"
                     type="password"
-                    label="Password"
-                    style={{marginTop: "40px"}}
+                    label="Mot de passe"
                     value={password}
                     onChange={(sender: any) => setPassword(sender.target.value)}
                 />
                 <TextField
+                    required
+                    className={classes.textField}
                     id="standard-password-input"
                     type="password"
-                    label="Password"
-                    style={{marginTop: "40px"}}
+                    label="Confirmer le mot de passe"
                     value={cPassword}
                     onChange={(sender: any) => setCPassword(sender.target.value)}
                 />
                 <div>
                     <Button
+                        disabled={!formValid}
                         color="primary"
-                        style={{ marginTop: "40px", justifyContent: "center" }}
+                        style={{ marginTop: "20px", justifyContent: "center" }}
                         onClick={() => {
-                            requestRegister(username, sha256(password), sha256(cPassword));
-                            props.setDisplayRegister(false);
+                            handleButtonClick();
+                            requestRegister(email, username, sha256(password), sha256(cPassword));
                         }}
                     >
-                        Register
+                        S'inscrire
                     </Button>
-                    <div>
-                        <Button
-                            color="primary"
-                            style={{ marginTop: "10px" }}
-                            onClick={() => props.setDisplayRegister(false)}
-                        >
-                            {"<"} Back to login
-                        </Button>
-                    </div>
+                </div>
+                <Divider variant={"middle"}/>
+                <div>
+                    <Button
+                        color="primary"
+                        style={{ marginTop: "20px", fontSize: "12px"}}
+                        onClick={() => props.setDisplayRegister(false)}
+                    >
+                        Vous avez déja un compte ? Connectez vous
+                    </Button>
                 </div>
             </div>
         </div>
